1729bbd82f7ae1ae5957d9cd77afa7ad480ab8c7
Hong Phuc <hongphuc.dthp@gmail.com>
2021-07-12 01:26:27 +0700

    Circle and bouncing

--- canvas.js
+++ canvas.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', () => {
 	const canvas = document.querySelector('#canvas');
 	const ctx = canvas.getContext('2d');
-	console.log(canvas);
 
 	//* Resizing
 	canvas.height = window.innerHeight;
@@ -9,8 +8,8 @@ window.addEventListener('load', () => {
 	//* ------
 
 	//* Rectangle
-  // ctx.fillStyle = 'blue'
-  // ctx.fillRect(50, 50, 50, 50)
+	// ctx.fillStyle = 'blue'
+	// ctx.fillRect(50, 50, 50, 50)
 
 	// ctx.lineWidth = 3
 	// ctx.strokeStyle = 'aqua'
@@ -27,11 +26,89 @@ window.addEventListener('load', () => {
 	// ctx.lineTo(200, 200)
 	// ctx.lineTo(400, 100)
 	// ctx.closePath()
+	// ctx.strokeStyle = 'rgba(0, 255, 0, 1)'
 	// ctx.stroke()
 	//* ------
 
-  //* Arc / Circle
-  //* ------
+	//* Arc / Circle
+	// ctx.beginPath()
+	// ctx.arc(500, 200, 100, 0, Math.PI * 2, false)
+	// ctx.strokeStyle = 'rgba(0, 0, 255, 1)'
+	// ctx.stroke()
+	//* ------
+
+	//* Randomize multiple circles
+	// for (let i = 0; i < 3; i++) {
+	//   let x = Math.random() * window.innerWidth
+	//   let y = Math.random() * window.innerHeight
+	//   let color = Math.floor(Math.random() * 16777215).toString(16)
+
+	//   ctx.beginPath()
+	//   ctx.arc(x, y, 100, 0, Math.PI * 2, false)
+	//   ctx.fillStyle = `#${color}`
+	//   ctx.fill()
+	// }
+	//* ------
+
+	//* Object
+	class Circle {
+		constructor(x, y, dx, dy, radius) {
+			this.x = x;
+			this.y = y;
+			this.radius = radius;
+
+			this.dx = dx;
+			this.dy = dy;
+
+			this.color = Math.floor(Math.random() * 16777215).toString(16);
+		}
+
+		draw() {
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+			ctx.fillStyle = `#${this.color}`;
+			ctx.fill();
+		}
+
+		update() {
+			if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+				this.dx = -this.dx;
+			}
+
+			if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+				this.dy = -this.dy;
+			}
+
+			this.x += this.dx;
+			this.y += this.dy;
+
+			this.draw();
+		}
+	}
+	//* -----
+
+	//* Animate
+
+	var circleArray = [];
+	for (let i = 0; i < 100; i++) {
+		let radius = 30;
+		let x = Math.random() * (innerWidth - radius * 2) + radius;
+		let y = Math.random() * (innerHeight - radius * 2) + radius;
+		let dx = Math.random() - 0.5;
+		let dy = Math.random() - 0.5;
+
+		circleArray.push(new Circle(x, y, dx, dy, radius));
+	}
+
+	function animate() {
+		requestAnimationFrame(animate);
+
+		ctx.clearRect(0, 0, innerWidth, innerHeight);
+		circleArray.forEach((circle) => circle.update());
+	}
+
+	animate();
+	//* ------
 
 	// Variables
 	// let painting = false
