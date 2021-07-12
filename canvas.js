767428dfc5a6f6775cf37342075d7fbc1cd893f6
Hong Phuc <hongphuc.dthp@gmail.com>
2021-07-12 09:00:51 +0700

    Draggable circle and placing

--- canvas.js
+++ canvas.js
@@ -1,60 +1,125 @@
+import { createRect } from './javascript/rectangle.mjs';
+import { drawLine } from './javascript/line.mjs';
+import { multipleCircles } from './javascript/circle.mjs';
+import { animate, initCircles } from './javascript/bouncing_circles.mjs';
 
-import { createRect } from './javascript/rectangle.mjs'
-import { drawLine } from './javascript/line.mjs'
-import { multipleCircles } from './javascript/circle.mjs'
-import { animate, initCircles } from './javascript/bouncing_circles.mjs'
+// Clear circles, set array to empty
+document.getElementById('buttonCanvas').onclick = () => {
+	ctx.clearRect(0, 0, innerWidth, innerHeight);
+	circles = [];
+};
 
-//* Resizing
-canvas.height = window.innerHeight;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+// Resizing
 canvas.width = window.innerWidth;
-//* ------
+canvas.height = window.innerHeight;
 
-//* Rectangle
-// createRect()
-//* ------
+// Event listeners
+window.addEventListener('mousemove', move);
+window.addEventListener('mousedown', setDraggable);
+window.addEventListener('mouseup', setDraggable);
 
-//* Line
-// drawLine()
-//* ------
+// Object
+class Circle {
+	constructor(x, y, radius, fill) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.fill = fill;
+	}
 
-//* Arc / Circle
-// multipleCircles()
-//* ------
-
-//* Arc / Circle
-initCircles()
-animate()
-//* ------
+	draw() {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+		ctx.fillStyle = this.fill;
+		ctx.fill();
+	}
+}
 
 // Variables
-// let painting = false
+var isMouseDown, circleMouseDown;
+var circles = [];
+var mousePosition = {
+	x: undefined,
+	y: undefined,
+};
+var focused = {
+	key: 0,
+	state: false,
+};
+var maxCircle = 5
 
-// Functions
-// function startPosition(e) {
-//   painting = true
-//   draw(e)
-// }
+// Handling functions
+function setDraggable(e) {
+	if (e.type === 'mousedown') {
+		isMouseDown = true;
+		if (circles.length < maxCircle) {
+			circleMouseDown = new Circle(e.clientX, e.clientY, 30, '#548CA8');
+			circles.push(circleMouseDown);
+		}
+	} else if (e.type === 'mouseup') {
+		circleMouseDown = null;
+		isMouseDown = false;
+		focused.state = false;
+	}
+}
 
-// function finishedPosition() {
-//   painting = false
-//   ctx.beginPath()
-// }
+function initCircle(e) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+  circles.forEach((circle) => {
+    circle.draw()
+    console.log(ctx.isPointInPath(e.x, e.y))
+  });
+}
 
-// function draw(e) {
-//   if (!painting) return;
+function move(e) {
+	if (!isMouseDown) return;
 
-//   ctx.lineWidth = 5
-//   ctx.lineCap = 'round'
-//   ctx.strokeStyle = 'aqua'
+  getMousePosition(e)
 
-//   ctx.lineTo(e.clientX, e.clientY)
-//   ctx.stroke()
-//   ctx.beginPath()
-//   ctx.moveTo(e.clientX, e.clientY)
-// }
+	if (focused.state) {
+		circles[focused.key].x = mousePosition.x;
+    circles[focused.key].y = mousePosition.y;
+		initCircle(e);
+		return;
+	}
 
-// Event Listeners
-// window.addEventListener('mousedown', startPosition)
-// window.addEventListener('mouseup', finishedPosition)
-// window.addEventListener('mousemove', draw)
+	for (var i = 0; i < circles.length; i++) {
+		if (intersects(circles[i])) {
+			circles.move(i, 0);
+			focused.state = true;
+			break;
+		}
+  }
 
+	initCircle(e);
+}
+
+function getMousePosition(e) {
+  var rect = canvas.getBoundingClientRect();
+  mousePosition = {
+      x: e.x - rect.left,
+      y: e.y - rect.top
+  }
+}
+
+function intersects(circle) {
+	var areaX = mousePosition.x - circle.x;
+	var areaY = mousePosition.y - circle.y;
+
+	return (
+		Math.pow(areaX, 2) + Math.pow(areaY, 2) <= circle.radius * circle.radius
+	);
+}
+
+Array.prototype.move = function (old_index, new_index) {
+	if (new_index >= this.length) {
+		var k = new_index - this.length;
+		while (k-- + 1) {
+			this.push(undefined);
+		}
+	}
+	this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+};
--- javascript/line.mjs
+++ javascript/line.mjs
@@ -1,12 +1,12 @@
 const ctx = canvas.getContext('2d');
 
 export function drawLine() {
-  ctx.beginPath()
-  ctx.moveTo(200, 400)
-  ctx.lineTo(200, 200)
-  ctx.lineTo(400, 100)
-  ctx.closePath()
+	ctx.beginPath();
+	ctx.moveTo(200, 400);
+	ctx.lineTo(200, 200);
+	ctx.lineTo(400, 100);
+	ctx.closePath();
 
-  ctx.strokeStyle = 'rgba(0, 255, 0, 1)'
-  ctx.stroke()
+	ctx.strokeStyle = 'rgba(0, 255, 0, 1)';
+	ctx.stroke();
 }
